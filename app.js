073b5c2c72790481489f78f15c5b2c32b393a8bb
RodriguesHUD <hudsondelimarodrigues@hotmail.com>
2023-06-23 00:22:21 -0300

    wip menu mobile

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ function scrollToSection(event) {
 linksInternos.forEach((link) => {
   link.addEventListener("click", scrollToSection);
 });
+
+const toggleBtn = document.querySelector(".burguerBtn");
+const toggleBtnIcon = document.querySelector(".burguerBtn img");
+const dropDownMenu = document.querySelector(".list-mobile");
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+};
+
+function changeIcon() {
+  var iconList = document.getElementById("iconList");
+  var iconX = document.getElementById("iconX");
+
+  iconList.classList.toggle("hidden");
+  iconX.classList.toggle("hidden");
+}
